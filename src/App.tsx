e0e5861e921deed67b51bfa6be3b92d3236edb41
Artem <rozen19942810@gmail.com>
2021-08-20 11:46:45 +0300

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 import { Background, Footer, Modal } from "./components";
 import {
