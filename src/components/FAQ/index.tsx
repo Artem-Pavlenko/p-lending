7cf35f186d4ff6c08ee258cd1e5590fe113365e6
Artem <rozen19942810@gmail.com>
2021-08-20 10:45:27 +0300

    feat: fonts

--- src/components/FAQ/index.tsx
+++ src/components/FAQ/index.tsx
@@ -3,33 +3,33 @@ import { FC } from "react";
 import IFAQProps from "./props";
 import "./styles.scss";
 import { Question } from "..";
+import { IMAGES } from "../../assets";
 
-export const FAQ: FC<IFAQProps> = () => {
-  return (
-    <div className="faq">
-      <p>FAQ</p>
-      <div className="questions">
-        <Question
-          number={1}
-          header="How I can join Panda?"
-          text="Lorem ipsum dolor sit amet, consectetur adipiscing "
-        />
-        <Question
-          number={2}
-          header="How I can join Panda?"
-          text="Lorem ipsum dolor sit amet, consectetur adipiscing "
-        />
-        <Question
-          number={3}
-          header="How I can join Panda?"
-          text="Lorem ipsum dolor sit amet, consectetur adipiscing "
-        />
-        <Question
-          number={4}
-          header="How I can join Panda?"
-          text="Lorem ipsum dolor sit amet, consectetur adipiscing "
-        />
-      </div>
+export const FAQ: FC<IFAQProps> = () => (
+  <div className="faq">
+    <p>FAQ</p>
+    <div className="questions">
+      <Question
+        number={1}
+        header="How I can join Panda?"
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing "
+      />
+      <Question
+        number={2}
+        header="How I can join Panda?"
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing "
+      />
+      <Question
+        number={3}
+        header="How I can join Panda?"
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing "
+      />
+      <Question
+        number={4}
+        header="How I can join Panda?"
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing "
+      />
+      <img className="footer_bamboo" src={IMAGES.BAMBOO} alt="" />
     </div>
-  );
-};
+  </div>
+);
